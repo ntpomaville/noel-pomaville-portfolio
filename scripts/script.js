f5bef7eee02de40693b8fc2a033adb27ba4785ea
Noel Pomaville <ntpomaville@gmail.com>
2020-11-09 16:00:42 -0500

    Organize and add some light colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,8 @@
-//https://discuss.dev.twitch.tv/t/streamers-live-status-on-website/8412/3
+//let portfolio = document.getElementById("portfolio")
+//let about = document.getElementById("about")
+//let contact = document.getElementById("contact")
+//let portrait = document.getElementById("portrait")
 
-let div = document.getElementById('body');
-let bottomBar = document.getElementById('bottom-bar');
-
-let hasVerticalScrollbar;
-
-hasVerticalScrollbar = div.scrollHeight > div.clientHeight;
-
-if (hasVerticalScrollbar == false) {
-    bottomBar.style.position = 'absolute';
-    bottomBar.style.bottom = '0';
-    bottomBar.style.right = '0';
-}
-
-if (hasVerticalScrollbar == true) {
-    bottomBar.style.position = 'static';
-}
-
-window.onresize = function moveBottomBarOnResize() {
-    hasVerticalScrollbar = div.scrollHeight > div.clientHeight;
-    if (hasVerticalScrollbar == false) {
-        bottomBar.style.position = 'absolute';
-        bottomBar.style.bottom = '0';
-        bottomBar.style.right = '0';
-    }
-    if (hasVerticalScrollbar == true) {
-        bottomBar.style.position = 'static';
-    }
-}
+//portfolio.addEventListener("mouseover", function() {
+//    portrait.
+//}
