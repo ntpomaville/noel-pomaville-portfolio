59c37d5b4e3a1aca3de1532fc7351b4ac4d3ff08
Noel Pomaville <ntpomaville@gmail.com>
2020-11-11 14:05:59 -0500

    Make main image color change on highlight, set selection spacing

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,28 @@
-//let portfolio = document.getElementById("portfolio")
-//let about = document.getElementById("about")
-//let contact = document.getElementById("contact")
-//let portrait = document.getElementById("portrait")
+let portfolio = document.getElementById("portfolio")
+let about = document.getElementById("about")
+let contact = document.getElementById("contact")
+let portrait = document.getElementById("portrait")
 
-//portfolio.addEventListener("mouseover", function() {
-//    portrait.
-//}
+portfolio.addEventListener("mouseover", function( event ) {
+    portrait.style.backgroundColor = "rgb(220,255,255)";
+}, false);
+
+portfolio.addEventListener("mouseout", function( event ) {
+    portrait.style.backgroundColor = "rgb(240,240,240)";
+}, false);
+
+about.addEventListener("mouseover", function( event ) {
+    portrait.style.backgroundColor = "rgb(220,220,255)";
+}, false);
+
+about.addEventListener("mouseout", function( event ) {
+    portrait.style.backgroundColor = "rgb(240,240,240)";
+}, false);
+
+contact.addEventListener("mouseover", function( event ) {
+    portrait.style.backgroundColor = "rgb(255,220,220)";
+}, false);
+
+contact.addEventListener("mouseout", function( event ) {
+    portrait.style.backgroundColor = "rgb(240,240,240)";
+}, false);
